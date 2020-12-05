d966fb5a4d6945c1c5f9a11e0d2806ec4dcf394a
kim eung gil <keg2606@homeplus.co.kr>
2020-12-05 23:20:40 +0900

    eslint / prettier 추가

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import reactImg from './assets/images/react.jpg';
 import reactSvg from './assets/images/react.svg';
 
 function App() {
-	console.log(process.env.NODE_ENV);
-	return (
-		<>
-			<div className='helloWebpack'>Hello Webpack</div>
-			<img src={reactSvg} alt='reactImg' className='reactImg' />
-			<img src={reactImg} alt='reactImg' className='reactImg' />
-		</>
-	);
+  console.log(process.env.NODE_ENV);
+  return (
+    <>
+      <div className="helloWebpack">Hello Webpack</div>
+      <img src={reactSvg} alt="reactImg" className="reactImg" />
+      <img src={reactImg} alt="reactImg" className="reactImg" />
+    </>
+  );
 }
 
 export default App;
